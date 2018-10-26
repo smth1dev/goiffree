672d682b0f5d810b0af68802fe78c98410d30ed4
smth1dev <smth1devweb@gmail.com>
2018-10-26 18:30:03 +0300

    delete

--- config.php
+++
@@ -1,9 +0,0 @@
- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
- <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>
-<?php
-
-//database_connection.php
-
-$connect = new PDO("mysql:host=localhost;dbname=goiffree_main", "root", "");
-
-?>
